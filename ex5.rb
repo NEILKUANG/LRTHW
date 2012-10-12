41c6e6a8d7fd0b2715de40dc5f6c3b5dfdd32c28
Neil Kuang <kuang.zhongrui@gmail.com>
2012-10-12 13:33:02 -0700

    Exercise

---
+++ ex5.rb
@@ -0,0 +1,15 @@
+my_name = 'Neil Kuang'
+my_age = 24 # not a lie
+my_height = 76 # inches
+my_weight = 200 # lb.
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+puts "Let's talk about %s." % my_name
+puts "He's %d inches tall." % my_height
+puts "He's %d pounds heavy." % my_weight
+puts "Actually that's not too heavy."
+puts "He's got %s eyes and %s hair." % [my_eyes, my_hair]
+puts "His teeth are usually %s depending on the coffee." % my_teeth
+puts "If I add %d, %d and %d I get %d." % [my_age, my_height, my_weight, my_age + my_height + my_weight]
